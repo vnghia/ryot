5aaecc61de8ba21d1e259e83284990731c77a40b
Vo Van Nghia <git@vnghia.com>
2024-08-09 07:16:53 +0200

    try ubuntu 22.04 again

--- ci/cross-pre-build.sh
+++ ci/cross-pre-build.sh
@@ -2,6 +2,13 @@
 set -euxo pipefail
 
 if [[ "$CROSS_TARGET" == "aarch64-unknown-linux-gnu" ]]; then
+  apt-get update && apt-get install --assume-yes libssl-dev build-essential
+
+  sed 's/^deb http/deb [arch=amd64] http/' -i '/etc/apt/sources.list'
+  echo 'deb [arch=arm64] http://archive.ubuntu.com/pub/ubuntu/ports jammy main restricted universe multiverse' >> /etc/apt/sources.list
+  echo 'deb [arch=arm64] http://archive.ubuntu.com/pub/ubuntu/ports jammy-updates main restricted universe multiverse' >> /etc/apt/sources.list
+  echo 'deb [arch=arm64] http://archive.ubuntu.com/pub/ubuntu/ports jammy-backports main restricted universe multiverse' >> /etc/apt/sources.list
+
   dpkg --add-architecture $CROSS_DEB_ARCH
-  apt-get update && apt-get --assume-yes install libssl-dev:$CROSS_DEB_ARCH
+  apt-get update && apt-get install --assume-yes libssl-dev:$CROSS_DEB_ARCH pkg-config:$CROSS_DEB_ARCH
 fi
