8bdf89d7c4de35842f0b8850d8e106a76dd2b1d9
Vo Van Nghia <git@vnghia.com>
2024-08-09 09:26:42 +0200

    build essential

--- ci/cross-pre-build.sh
+++ ci/cross-pre-build.sh
@@ -2,12 +2,12 @@
 set -euxo pipefail
 
 if [[ "$CROSS_TARGET" == "aarch64-unknown-linux-gnu" ]]; then
-  apt-get update && apt-get install --assume-yes libssl-dev pkg-config
+  apt-get update && apt-get install --assume-yes libssl-dev build-essential pkg-config
 
   sed 's/^deb http/deb [arch=amd64] http/' -i '/etc/apt/sources.list'
-  echo 'deb [arch=arm64] http://ports.ubuntu.com/ jammy main restricted universe multiverse' >>/etc/apt/sources.list
-  echo 'deb [arch=arm64] http://ports.ubuntu.com/ jammy-updates main restricted universe multiverse' >>/etc/apt/sources.list
-  echo 'deb [arch=arm64] http://ports.ubuntu.com/ jammy-backports main restricted universe multiverse' >>/etc/apt/sources.list
+  echo 'deb [arch=arm64] http://ports.ubuntu.com/ jammy main restricted universe multiverse' >> /etc/apt/sources.list
+  echo 'deb [arch=arm64] http://ports.ubuntu.com/ jammy-updates main restricted universe multiverse' >> /etc/apt/sources.list
+  echo 'deb [arch=arm64] http://ports.ubuntu.com/ jammy-backports main restricted universe multiverse' >> /etc/apt/sources.list
 
   dpkg --add-architecture $CROSS_DEB_ARCH
   apt-get update && apt-get install --assume-yes libssl-dev:$CROSS_DEB_ARCH pkg-config:$CROSS_DEB_ARCH
